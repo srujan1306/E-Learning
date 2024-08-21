bcda81ecb4052b31a47c871258bf5e9c3d0bc541
srujan1306 <srujan.pothu@proclink.com>
2024-08-21 12:14:02 +0530

    home page modified successfully

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,16 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
-
+// const API = 'https://66b0acbf6a693a95b539b996.mockapi.io';
+// const API = 'http://localhost:4000';
+const API = 'https://e-learn-nodejs-backend.onrender.com';
 @Injectable({
   providedIn: 'root',
 })
 export class SearchService {
   constructor(private http: HttpClient) {}
   searchCourse(searchTerm: string) {
-    return this.http.get(
-      `https://66b0acbf6a693a95b539b996.mockapi.io/Elearn?search=${searchTerm}`
-    );
-    // return this.http.get(`http://localhost:4000/Elearn?search=${searchTerm}`);
+    return this.http.get(`${API}/Elearn?search=${searchTerm}`);
   }
 }
